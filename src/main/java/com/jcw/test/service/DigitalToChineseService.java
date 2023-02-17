85d41b833c441680948874e5931b401bb51db253
jcw <2846519731@qq.com>
2023-02-17 16:41:16 +0800

    数字转文字

--- src/main/java/com/jcw/test/service/DigitalToChineseService.java
+++ src/main/java/com/jcw/test/service/DigitalToChineseService.java
@@ -10,7 +10,7 @@ public class DigitalToChineseService {
         String[] unit = {"", "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千","兆"};
 
         String currentUnit;
-        String str = new String();
+        StringBuilder str = new StringBuilder();
 
         int[] ints = StringToInt(data,length);
         String[] strings = To(ints);
@@ -18,13 +18,13 @@ public class DigitalToChineseService {
         int count = 0;
         for (int i = length; i > 0; i--) {
             currentUnit = unit[i-1];
-            str+=strings[count];
+            str.append(strings[count]);
             if (!"零".equals(strings[count])){
-                str+=currentUnit;
+                str.append(currentUnit);
             }
             count++;
         }
-        return str;
+        return str.toString();
     }
 
     public static int[] StringToInt(String s,int length){
@@ -39,36 +39,17 @@ public class DigitalToChineseService {
     public static String[] To(int[] ints){
         String[] strings = new String[ints.length];
         for (int i = 0; i < ints.length; i++) {
-            switch (ints[i]){
-                case 0:
-                    strings[i] = MyEnum.Ling.getData();
-                    break;
-                case 1:
-                    strings[i] = MyEnum.YI.getData();
-                    break;
-                case 2:
-                    strings[i] = MyEnum.ER.getData();
-                    break;
-                case 3:
-                    strings[i] = MyEnum.SAN.getData();
-                    break;
-                case 4:
-                    strings[i] = MyEnum.SI.getData();
-                    break;
-                case 5:
-                    strings[i] = MyEnum.WU.getData();
-                    break;
-                case 6:
-                    strings[i] = MyEnum.LIU.getData();
-                    break;
-                case 7:
-                    strings[i] = MyEnum.QI.getData();
-                    break;
-                case 8:
-                    strings[i] = MyEnum.BA.getData();
-                    break;
-                case 9:
-                    strings[i] = MyEnum.JIU.getData();
+            switch (ints[i]) {
+                case 0 -> strings[i] = MyEnum.Ling.getData();
+                case 1 -> strings[i] = MyEnum.YI.getData();
+                case 2 -> strings[i] = MyEnum.ER.getData();
+                case 3 -> strings[i] = MyEnum.SAN.getData();
+                case 4 -> strings[i] = MyEnum.SI.getData();
+                case 5 -> strings[i] = MyEnum.WU.getData();
+                case 6 -> strings[i] = MyEnum.LIU.getData();
+                case 7 -> strings[i] = MyEnum.QI.getData();
+                case 8 -> strings[i] = MyEnum.BA.getData();
+                case 9 -> strings[i] = MyEnum.JIU.getData();
             }
         }
         return strings;
